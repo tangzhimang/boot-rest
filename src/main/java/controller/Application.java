a2c43930019437ca7113212818772c6005953c2d
tangzhimang <244327337@qq.com>
2017-09-19 17:55:47 +0800

    增加修改tomcat端口代码

--- src/main/java/controller/Application.java
+++ src/main/java/controller/Application.java
@@ -2,15 +2,24 @@ package controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class Application {
+public class Application implements EmbeddedServletContainerCustomizer{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 SpringApplication.run(Application.class, args);
 
+	}
+	
+	
+	//更改Tomcat端口
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		container.setPort(8088);
 	}
 
 }
